0adaf015e427b4054c39e3e0abd44ebe72151b5c
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-06-23 16:13:51 -0400

    Update getModels.php
    * Refactor model parsing logic
    * Add error handlers

--- src/scripts/getModels.php
+++ src/scripts/getModels.php
@@ -1,38 +1,57 @@
 <?php
 
 //Change Directory to workspace
-chdir($argv[1]);
+if (!is_dir($workspace = $argv[1])) {
+    error_output("'{$workspace}' is not a valid directory", 2);
+}
+
+chdir($workspace);
 
 require_once __DIR__ . '/helpers.php';
-$loader = require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 //Bootstrap App
 (require_once 'bootstrap/app.php')
     ->make(Illuminate\Contracts\Console\Kernel::class)
     ->bootstrap();
 
-//Get project classes
-$classes = $loader->getClassMap();
+//Register Error Handlers
+set_exception_handler(fn(Throwable $e) => error_output("Runtime exception - {$e->getMessage()} {$e->getFile()} {$e->getLine()}", $e->getCode() ?: 1));
 
-$composer = json_decode(file_get_contents("composer.json"), true);
-$projectNamespaces = array_keys($composer['autoload']['psr-4']);
+set_error_handler(fn(int $errno, string $errstr) => error_output("Runtime error - {$errstr}", match ($errno) {
+    E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR => 255,
+    E_NOTICE, E_USER_NOTICE, E_STRICT, E_DEPRECATED, E_USER_DEPRECATED => 1,
+    default => 7
+}));
 
-$projectClasses = array_filter($classes, mode: ARRAY_FILTER_USE_KEY, callback: function (string $class) use ($projectNamespaces) {
-    foreach ($projectNamespaces as $namespace) if (str_starts_with($class, $namespace)) return true;
-    return false;
+register_shutdown_function(function () {
+    $e = error_get_last();
+    if (!$e || !in_array($e['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+        return;
+    }
+    error_output("Critic error - {$e['type']}", 255);
 });
 
-//Filter models from classes
-$models = array_filter(
-    $projectClasses,
-    mode: ARRAY_FILTER_USE_BOTH,
-    callback: fn(string $file, string $class) =>
-        is_string($class) &&
-        is_string($file) &&
-        class_extends_model($file, $class)
-);
+//Get project classes
+try {
+    $projectNamespaces = json_decode(file_get_contents("composer.json"), true, flags: JSON_THROW_ON_ERROR)['autoload']['psr-4'];
+} catch (\ErrorException $e) {
+    throw new RuntimeException("'composer.json' file is missing.", 4);
+}
+
+$models = [];
+
+foreach ($projectNamespaces as $namespacePath) {
+    foreach (find_php_files($namespacePath) as $filePath) {
+        $class = get_fully_qualified_class_name($filePath);
+        if (is_null($class) || !class_extends_model($filePath)) {
+            continue;
+        }
+        $models[] = $class;
+    }
+}
 
 //Parse models into extension structure
-$modelsInfo = array_map(array: array_keys($models), callback: fn(string $class) => parseModelInfo($class));
+$modelsInfo = array_map(array: $models, callback: fn(string $class) => parse_model_info($class));
 
 json_output($modelsInfo);
