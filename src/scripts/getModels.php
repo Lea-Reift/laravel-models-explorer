55e87190558c9a714e433c8921d05381443c34d1
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-06-22 23:06:51 -0400

    Show only main project models

--- src/scripts/getModels.php
+++ src/scripts/getModels.php
@@ -3,28 +3,36 @@
 //Change Directory to workspace
 chdir($argv[1]);
 
-require_once __DIR__.'/helpers.php';
+require_once __DIR__ . '/helpers.php';
 $loader = require_once 'vendor/autoload.php';
-
-//Get project classes
-$classes = $loader->getClassMap();
 
 //Bootstrap App
 (require_once 'bootstrap/app.php')
     ->make(Illuminate\Contracts\Console\Kernel::class)
     ->bootstrap();
 
+//Get project classes
+$classes = $loader->getClassMap();
+
+$composer = json_decode(file_get_contents("composer.json"), true);
+$projectNamespaces = array_keys($composer['autoload']['psr-4']);
+
+$projectClasses = array_filter($classes, mode: ARRAY_FILTER_USE_KEY, callback: function (string $class) use ($projectNamespaces) {
+    foreach ($projectNamespaces as $namespace) if (str_starts_with($class, $namespace)) return true;
+    return false;
+});
+
 //Filter models from classes
 $models = array_filter(
-    $classes,
+    $projectClasses,
     mode: ARRAY_FILTER_USE_BOTH,
-    callback: fn (string $file, string $class) => is_string($class) &&
+    callback: fn(string $file, string $class) =>
+        is_string($class) &&
         is_string($file) &&
-        ! str_starts_with($class, 'Illuminate\\Database\\Eloquent') &&
         class_extends_model($file, $class)
 );
 
 //Parse models into extension structure
-$modelsInfo = array_map(array: array_keys($models), callback: fn (string $class) => parseModelInfo($class));
+$modelsInfo = array_map(array: array_keys($models), callback: fn(string $class) => parseModelInfo($class));
 
 json_output($modelsInfo);
