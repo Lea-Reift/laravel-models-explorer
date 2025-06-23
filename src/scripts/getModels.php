1df693bc48dac056de7a3293c65f0fcd53dd71b4
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-06-22 22:49:20 -0400

    Refactoring getModels script

--- src/scripts/getModels.php
+++ src/scripts/getModels.php
@@ -1,113 +1,30 @@
 <?php
+
 //Change Directory to workspace
-[1 => $workspace] = $argv;
-chdir($workspace);
+chdir($argv[1]);
 
-require_once "vendor/autoload.php";
+require_once __DIR__.'/helpers.php';
+$loader = require_once 'vendor/autoload.php';
 
-// Parse composer namespaces' classes
-$composer = json_decode(file_get_contents("composer.json"), true);
-$namespaces = $composer['autoload']['psr-4'];
-$classes = [];
+//Get project classes
+$classes = $loader->getClassMap();
 
-foreach ($namespaces as $namespace => $namespacePath) {
-    $namespace = rtrim($namespace, "\\");
-    $classes = array_merge($classes, findModelClasses($namespace, $namespacePath));
-}
-
-// Enable Laravel kernel
-(require_once "bootstrap/app.php")
+//Bootstrap App
+(require_once 'bootstrap/app.php')
     ->make(Illuminate\Contracts\Console\Kernel::class)
     ->bootstrap();
 
-// Return output as json
-echo json_encode(array_map(array: $classes, callback: 'parseModelInfo'), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-exit;
+//Filter models from classes
+$models = array_filter(
+    $classes,
+    mode: ARRAY_FILTER_USE_BOTH,
+    callback: fn (string $file, string $class) => is_string($class) &&
+        is_string($file) &&
+        ! str_starts_with($class, 'Illuminate\\Database\\Eloquent') &&
+        class_extends_model($file, $class)
+);
 
-// Helper functions
-if (! function_exists('class_basename')) {
-    function class_basename(string|object $class)
-    {
-        $class = is_object($class) ? get_class($class) : $class;
+//Parse models into extension structure
+$modelsInfo = array_map(array: array_keys($models), callback: fn (string $class) => parseModelInfo($class));
 
-        return basename(str_replace('\\', '/', $class));
-    }
-}
-
-function parseModelRelations(Illuminate\Database\Eloquent\Model $model): array
-{
-    $reflection = new ReflectionClass($model);
-    $methods = $reflection->getMethods();
-    $relations = [];
-
-    foreach ($methods as $metodo) {
-        // Saltar métodos heredados
-        if ($metodo->class !== get_class($model)) {
-            continue;
-        }
-
-        // Ignorar métodos con parámetros
-        if ($metodo->getNumberOfParameters() > 0) {
-            continue;
-        }
-
-        $resultado = $metodo->invoke($model);
-        if ($resultado instanceof Illuminate\Database\Eloquent\Relations\Relation) {
-            $relations[] = [
-                'name' => $metodo->getName(),
-                'type' => class_basename($resultado),
-                'relatedModel' => get_class($resultado->getRelated())
-            ];
-        }
-    }
-
-    return $relations;
-}
-
-
-function parseModelInfo(string $modelClass): array
-{
-    $model = new $modelClass();
-
-    $reflection = new ReflectionClass($model);
-
-    return [
-        'name' => $reflection->getShortName(),
-        'namespace' => $reflection->getNamespaceName(),
-        'table' => $model->getTable(),
-        'fillable' => $model->getFillable(),
-        'hidden' => $model->getHidden(),
-        'casts' => $model->getCasts(),
-        'relationships' => parseModelRelations($model),
-        'traits' => $reflection->getTraitNames(),
-        'uri' => $reflection->getFileName(),
-    ];
-}
-
-function findModelClasses(string $namespace, string $path): array
-{
-    $directory = new DirectoryIterator($path);
-    $classes = [];
-
-    foreach ($directory as $item) {
-        if ($item->isDot()) {
-            continue;
-        }
-
-        if ($item->isDir()) {
-            $classes = [...$classes, ...findModelClasses("{$namespace}\\{$item->getBasename()}\\", $item->getRealPath())];
-        }
-
-        if (!$item->isFile()) {
-            continue;
-        }
-
-        $class = str_replace($directory->getPath(), $namespace, $item->getPath()) . $item->getBasename(".php");
-
-        if (is_subclass_of($class, Illuminate\Database\Eloquent\Model::class)) {
-            $classes[] = $class;
-        }
-    }
-
-    return $classes;
-}
+json_output($modelsInfo);
