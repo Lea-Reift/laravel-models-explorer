68d4ffd6053d1036e67e9954f27e6ef555accaff
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-06-23 22:10:13 -0400

    Enable support for older php | laravel versions
    * Making minimum php version 7.4+
    * Making minimum laravel version 8

--- src/scripts/getModels.php
+++ src/scripts/getModels.php
@@ -1,44 +1,71 @@
 <?php
+require_once __DIR__ . '/helpers.php';
+
+if (PHP_MAJOR_VERSION < 7 || (PHP_MAJOR_VERSION == 7 && PHP_MINOR_VERSION < 4)) {
+    $currentVersion = PHP_VERSION;
+    error_output("Version error - PHP version 7.4 or greater is required to run this script (current version is {$currentVersion})", 1);
+}
 
 //Change Directory to workspace
 if (!is_dir($workspace = $argv[1])) {
-    error_output("'{$workspace}' is not a valid directory", 2);
+    error_output("Argument exception -'{$workspace}' is not a valid directory", 2);
 }
 
 chdir($workspace);
 
-require_once __DIR__ . '/helpers.php';
 require_once 'vendor/autoload.php';
+
+if (($currentVersion = (float)Illuminate\Foundation\Application::VERSION) < 8) {
+    error_output("Version error - Laravel version 8 or greater is required to run this script (current version is {$currentVersion})", 1);
+}
 
 //Bootstrap App
 (require_once 'bootstrap/app.php')
     ->make(Illuminate\Contracts\Console\Kernel::class)
     ->bootstrap();
 
-//Register Error Handlers
-set_exception_handler(fn(Throwable $e) => error_output("Runtime exception - {$e->getMessage()} {$e->getFile()} {$e->getLine()}", $e->getCode() ?: 1));
 
-set_error_handler(fn(int $errno, string $errstr) => error_output("Runtime error - {$errstr}", match ($errno) {
-    E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR => 255,
-    E_NOTICE, E_USER_NOTICE, E_STRICT, E_DEPRECATED, E_USER_DEPRECATED => 1,
-    default => 7
-}));
+//Register Error Handlers
+set_exception_handler(fn(Throwable $e) => error_output("Runtime Exception - {$e->getMessage()}", $e->getCode() ?: 1));
+
+set_error_handler(function (int $errno, string $errstr) {
+    $errorCode = 1;
+    switch ($errno) {
+        case E_ERROR:
+        case E_PARSE:
+        case E_CORE_ERROR:
+        case E_COMPILE_ERROR:
+            $errorCode = 255;
+            break;
+        case E_NOTICE:
+        case E_USER_NOTICE:
+        case E_STRICT:
+        case E_DEPRECATED:
+        case E_USER_DEPRECATED:
+            $errorCode = 1;
+            break;
+        default:
+            $errorCode = 7;
+            break;
+    }
+    file_put_contents(__DIR__ . "/var.php", "<?php\nreturn " . var_export(debug_backtrace(), true));
+    error_output("Runtime Error - {$errstr}", $errorCode);
+});
 
 register_shutdown_function(function () {
     $e = error_get_last();
     if (!$e || !in_array($e['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
         return;
     }
-    error_output("Critic error - {$e['type']}", 255);
+    error_output("Critical Error - {$e['message']}", 255);
 });
 
 //Get project classes
 try {
-    $projectNamespaces = json_decode(file_get_contents("composer.json"), true, flags: JSON_THROW_ON_ERROR)['autoload']['psr-4'];
+    $projectNamespaces = json_decode(file_get_contents("composer.json"), true, 512, JSON_THROW_ON_ERROR)['autoload']['psr-4'];
 } catch (\ErrorException $e) {
     throw new RuntimeException("'composer.json' file is missing.", 4);
 }
-
 $models = [];
 
 foreach ($projectNamespaces as $namespacePath) {
@@ -52,6 +79,6 @@ foreach ($projectNamespaces as $namespacePath) {
 }
 
 //Parse models into extension structure
-$modelsInfo = array_map(array: $models, callback: fn(string $class) => parse_model_info($class));
+$modelsInfo = array_map(fn(string $class) => parse_model_info($class), $models);
 
 json_output($modelsInfo);
--- src/scripts/helpers.php
+++ src/scripts/helpers.php
@@ -2,42 +2,46 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-function json_output(mixed $data): never
-{
-    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    exit(0);
+if (! function_exists('json_output')) {
+    function json_output($data): never
+    {
+        echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        exit(0);
+    }
 }
 
-function error_output(string $message, int $code = 1): never
-{
-    fwrite(STDERR, $message . PHP_EOL);
-    exit($code);
+if (! function_exists('error_output')) {
+    function error_output($message, $code = 1): never
+    {
+        fwrite(STDERR, $message . PHP_EOL);
+        exit($code);
+    }
 }
 
 if (! function_exists('array_find')) {
-    function array_find(array $array, callable $callback, string $use = 'BOTH'): mixed
+    function array_find($array, $callback, $use = 'BOTH')
     {
         if (! in_array($use, ['KEY', 'VALUE', 'BOTH'])) {
             throw new InvalidArgumentException("Invalid use {$use} in array_find");
         }
 
         foreach ($array as $key => $value) {
+            $result = false;
             switch ($use) {
-                case 'KEY':
-                    if ($callback($key)) {
-                        return $value;
-                    }
-                    break;
                 case 'VALUE':
-                    if ($callback($value)) {
-                        return $value;
-                    }
+                    $result = $callback($value);
+                    break;
+
+                case 'KEY':
+                    $result = $callback($key);
                     break;
                 default:
-                    if ($callback($value, $key)) {
-                        return $value;
-                    }
+                    $result = $callback($value, $key);
                     break;
+            }
+
+            if ($result) {
+                return $value;
             }
         }
 
@@ -45,123 +49,175 @@ if (! function_exists('array_find')) {
     }
 }
 
-function class_extends_model(string $classFilePath, ?string $class = null): bool
-{
-    if (! is_file($classFilePath)) {
-        return false;
-    }
-
-    $class ??= get_fully_qualified_class_name($classFilePath);
-
-    if (!class_exists($class)) {
-        require_once $classFilePath;
-    }
-
-    return is_subclass_of($class, Model::class);
-}
-
-function get_default_php_token(string|int|null|PhpToken $token, array $tokensArray = []): PhpToken
-{
-    $defaultToken = new PhpToken(T_WHITESPACE, " ");
-
-    return match (true) {
-        is_a($token, PhpToken::class) => $token,
-        is_string($token) || is_int($token) => $tokensArray[$token] ?? $defaultToken,
-        default => $defaultToken
-    };
-}
-
-function get_fully_qualified_class_name(string $classFilePath): ?string
-{
-    if (! is_file($classFilePath)) {
-        return false;
-    }
-
-    $code = file_get_contents($classFilePath);
-
-    if ($code === false) {
-        return false;
-    }
-
-    $tokens = PhpToken::tokenize($code);
-    $namespace = array_find($tokens, use: 'KEY', callback: fn(int $key) => get_default_php_token($key - 2, $tokens)->is(T_NAMESPACE))?->text;
-    $currentClass = array_find($tokens, use: 'KEY', callback: fn(int $key) => get_default_php_token($key - 2, $tokens)->is(T_CLASS))?->text;
-
-    return $namespace ? "$namespace\\$currentClass" : $currentClass;
-}
-
-function parse_model_relations(Model $model): array
-{
-    $reflection = new ReflectionClass($model);
-
-    $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
-    $relations = [];
-
-    foreach ($methods as $method) {
-        $type = null;
-        if ($method->class !== get_class($model)) {
-            continue;
+if (! function_exists('class_extends_model')) {
+    function class_extends_model($classFilePath, $class = null): bool
+    {
+        if (! is_file($classFilePath)) {
+            return false;
         }
 
-        if ($method->getNumberOfParameters() > 0) {
-            continue;
+        $class ??= get_fully_qualified_class_name($classFilePath);
+
+        if (!class_exists($class)) {
+            require_once $classFilePath;
         }
 
-        if (! $method->hasReturnType() || $method->getReturnType()->isBuiltin()) {
-            $docComment = $method->getDocComment();
-            if ($docComment === false) {
+        return is_subclass_of($class, Model::class);
+    }
+}
+
+if (! function_exists('get_default_php_token')) {
+    function get_default_php_token($token, $tokensArray = []): PhpToken
+    {
+        $defaultToken = new PhpToken(T_WHITESPACE, " ");
+
+        switch (true) {
+            case is_a($token, PhpToken::class):
+                return $token;
+                break;
+
+            case is_string($token) || is_int($token):
+                return $tokensArray[$token] ?? $defaultToken;
+                break;
+
+            default:
+                return $defaultToken;
+                break;
+        }
+    }
+}
+
+if (! function_exists('get_fully_qualified_class_name')) {
+    function get_fully_qualified_class_name($classFilePath): ?string
+    {
+        if (! is_file($classFilePath)) {
+            return null;
+        }
+
+        $code = file_get_contents($classFilePath);
+
+        if ($code === false) {
+            return null;
+        }
+
+        $tokens = PhpToken::tokenize($code);
+        $currentClass = array_find($tokens, fn($key) => get_default_php_token($key - 2, $tokens)->is(T_CLASS), 'KEY');
+
+        if (is_null($currentClass)) {
+            return null;
+        }
+
+        $currentClass = $currentClass->text;
+        $tokensByLine = array_group_by($tokens, fn(PhpToken $token) => $token->line);
+
+        $namespace = array_find($tokens, fn($key) => get_default_php_token($key - 2, $tokens)->is(T_NAMESPACE), 'KEY');
+
+        if (!is_null($namespace)) {
+            $namespaceLineTokens = array_filter(
+                $tokensByLine[$namespace->line],
+                fn(PhpToken $token) => !$token->is(T_WHITESPACE) && !$token->is(T_NAMESPACE) && !$token->is(59) //Semicolon
+            );
+
+            $namespaceLine = array_map(fn(PhpToken $token) => $token->text, $namespaceLineTokens);
+            $namespace = implode("", $namespaceLine);
+        }
+
+        return $namespace ? "$namespace\\$currentClass" : $currentClass;
+    }
+}
+
+if (!function_exists('array_group_by')) {
+    function array_group_by($array, $groupBy)
+    {
+        $result = [];
+
+        foreach ($array as $key => $value) {
+            $groupByKey = is_callable($groupBy) ? $groupBy($value, $key) : $groupBy;
+            $result[$groupByKey][] = $value;
+        }
+
+        return $result;
+    }
+}
+
+if (! function_exists('parse_model_relations')) {
+    function parse_model_relations(Model $model): array
+    {
+        $reflection = new ReflectionClass($model);
+
+        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+        $relations = [];
+
+        foreach ($methods as $method) {
+            $type = null;
+            if ($method->class !== get_class($model)) {
                 continue;
             }
 
-            if (! preg_match('/@return\s+(?<type>[^\s]+)/', $docComment, $match)) {
+            if ($method->getNumberOfParameters() > 0) {
                 continue;
             }
 
-            $type = $match['type'];
+            if (! $method->hasReturnType() || $method->getReturnType()->isBuiltin()) {
+                $docComment = $method->getDocComment();
+                if ($docComment === false) {
+                    continue;
+                }
+
+                if (! preg_match('/@return\s+(?<type>[^\s]+)/', $docComment, $match)) {
+                    continue;
+                }
+
+                $type = $match['type'];
+            }
+
+            $type ??= $method->getReturnType()->getName();
+
+            if (! is_subclass_of($type, Illuminate\Database\Eloquent\Relations\Relation::class)) {
+                continue;
+            }
+
+            $relation = $method->invoke($model);
+
+            $relations[] = [
+                'name' => $method->getName(),
+                'type' => basename(get_class($relation)),
+                'relatedModel' => get_class($relation->getRelated()),
+            ];
         }
 
-        $type ??= $method->getReturnType()->getName();
+        return $relations;
+    }
+}
 
-        if (! is_subclass_of($type, Illuminate\Database\Eloquent\Relations\Relation::class)) {
-            continue;
-        }
+if (! function_exists('parse_model_info')) {
+    function parse_model_info($modelClass): array
+    {
+        $model = new $modelClass();
 
-        $relation = $method->invoke($model);
+        $reflection = new ReflectionClass($model);
 
-        $relations[] = [
-            'name' => $method->getName(),
-            'type' => basename($relation::class),
-            'relatedModel' => get_class($relation->getRelated()),
+        return [
+            'name' => $reflection->getShortName(),
+            'namespace' => $reflection->getNamespaceName(),
+            'table' => $model->getTable(),
+            'fillable' => $model->getFillable(),
+            'hidden' => $model->getHidden(),
+            'casts' => $model->getCasts(),
+            'relationships' => parse_model_relations($model),
+            'traits' => $reflection->getTraitNames(),
+            'uri' => $reflection->getFileName(),
         ];
     }
-
-    return $relations;
 }
 
-function parse_model_info(string $modelClass): array
-{
-    $model = new $modelClass();
+if (! function_exists('find_php_files')) {
+    function find_php_files($dir)
+    {
+        $directory = new RecursiveDirectoryIterator($dir);
+        $flattened = new RecursiveIteratorIterator($directory);
+        $files = iterator_to_array(new RegexIterator($flattened, '/^.+\.php$/i'));
 
-    $reflection = new ReflectionClass($model);
-
-    return [
-        'name' => $reflection->getShortName(),
-        'namespace' => $reflection->getNamespaceName(),
-        'table' => $model->getTable(),
-        'fillable' => $model->getFillable(),
-        'hidden' => $model->getHidden(),
-        'casts' => $model->getCasts(),
-        'relationships' => parse_model_relations($model),
-        'traits' => $reflection->getTraitNames(),
-        'uri' => $reflection->getFileName(),
-    ];
-}
-
-function find_php_files($dir)
-{
-    $directory = new RecursiveDirectoryIterator($dir);
-    $flattened = new RecursiveIteratorIterator($directory);
-    $files = iterator_to_array(new RegexIterator($flattened, '/^.+\.php$/i'));
-
-    return array_keys($files);
+        return array_keys($files);
+    }
 }
